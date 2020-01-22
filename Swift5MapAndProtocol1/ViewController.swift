d559f95d306f9d654e1bb0281ce079528a2286c5
yuma11 <11yuma20@gmail.com>
2020-01-22 18:57:44 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol1
+//
+//  Created by yuma on 2020/01/22.
+//  Copyright © 2020 Yuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
